87027689835aeb7fe324367f2c824d8316d33998
Timm <timmschmidt7@t-online.de>
2023-09-11 22:06:10 +0200

    Basic Event System
    Former-commit-id: 1151b5c1321df6d5693231a462698eb9874feafe

--- 3DCast/3DCast/src/3DCast/Application.cpp
+++ 3DCast/3DCast/src/3DCast/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "3DCast/Event/ApplicationEvent.h"
+#include "3DCast/Log.h"
+
 Cast::Application::Application()
 {
 }
@@ -10,5 +13,9 @@ Cast::Application::~Application()
 
 void Cast::Application::Run()
 {
+	LOG_INFO("This is an event-test!");
+	WindowResizeEvent e(1020, 900);
+	LOG_INFO(e.ToString());
+
 	while (true);
 }
--- 3DCast/3DCast/src/3DCast/EntryPoint.h
+++ 3DCast/3DCast/src/3DCast/EntryPoint.h
@@ -6,7 +6,6 @@ extern Cast::Application* Cast::CreatApplication();
 
 int main(int argc, char** argv) {
 	Cast::Log::Init();
-	Cast::Log::GetCoreLogger()->warn("Initialized Logging");
 	Cast::Log::GetClientLogger()->info("Initialized Logging");
 
 	auto app = Cast::CreatApplication();
