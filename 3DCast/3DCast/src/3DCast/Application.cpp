ec59d5fa41efffde5fab816a1358e7dcead92651
Timm <timmschmidt7@t-online.de>
2023-09-17 14:46:17 +0200

    Window Events

--- 3DCast/3DCast/src/3DCast/Application.cpp
+++ 3DCast/3DCast/src/3DCast/Application.cpp
@@ -1,12 +1,14 @@
 #include "castpch.h"
 #include "Application.h"
 
-#include "3DCast/Event/ApplicationEvent.h"
 #include "3DCast/Log.h"
+
+#define BIND_EVENT_FUNC(x) std::bind(&Cast::Application::x, this, std::placeholders::_1)
 
 Cast::Application::Application()
 {
 	m_Window = std::unique_ptr<Window>(Window::Create());
+	m_Window->SetEventCallback(BIND_EVENT_FUNC(OnEvent));
 }
 
 Cast::Application::~Application()
@@ -15,11 +17,21 @@ Cast::Application::~Application()
 
 void Cast::Application::Run()
 {
-	LOG_INFO("This is an event-test!");
-	WindowResizeEvent e(1020, 900);
-	LOG_INFO(e.ToString()); // Need to revisit
-
 	while (m_Running) {
 		m_Window->OnUpdate();
 	}
+}
+
+void Cast::Application::OnEvent(Event& e)
+{
+	EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FUNC(OnWindowClose));
+
+	LOG_CORE_TRACE("{0}", e.ToString());
+}
+
+bool Cast::Application::OnWindowClose(WindowCloseEvent& e)
+{
+	m_Running = false;
+	return true;
 }
--- 3DCast/3DCast/src/3DCast/Application.h
+++ 3DCast/3DCast/src/3DCast/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Event/Event.h"
 #include "Window.h"
+#include "3DCast/Event/ApplicationEvent.h"
 
 namespace Cast {
 	class CAST_API Application
@@ -12,6 +13,10 @@ namespace Cast {
 		~Application();
 
 		void Run();
+		void OnEvent(Event& e);
+
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 
 	private:
 		std::unique_ptr<Window> m_Window;
